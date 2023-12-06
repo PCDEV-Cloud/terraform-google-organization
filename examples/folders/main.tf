5e165a171f40b440d1753a8dc3ba31ec89234314
Paweł Cholewa <pawel.cholewa@pcdev.pl>
2023-12-06 13:17:53 +0100

    Add folders submodule example

---
+++ examples/folders/main.tf
@@ -0,0 +1,25 @@
+provider "google" {}
+
+module "folders" {
+  source = "../../modules/folders"
+
+  parent = "organizations/123456789123"
+
+  folders = [
+    {
+      display_name = "Security"
+      folders = [
+        { display_name = "Prod" },
+        { display_name = "SDLC" }
+      ]
+    },
+    {
+      display_name = "Infrastructure"
+      folders = [
+        { display_name = "Prod" },
+        { display_name = "SDLC" }
+      ]
+    },
+    { display_name = "Projects" }
+  ]
+}
