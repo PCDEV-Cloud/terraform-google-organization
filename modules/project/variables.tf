4e3a5803ff65520606f2fccbd6e2804b5e10a0ef
Paweł Cholewa <pawel.cholewa@pcdev.pl>
2023-12-07 11:32:14 +0100

    Add variables validation and description

--- modules/project/variables.tf
+++ modules/project/variables.tf
@@ -1,11 +1,11 @@
 variable "name" {
   type        = string
-  description = ""
+  description = "The display name of the project."
 }
 
 variable "project_id" {
   type        = string
-  description = ""
+  description = "The project ID. Changing this forces a new project to be created."
 
   validation {
     condition     = can(regex("^[a-z][a-z0-9-]{4,28}[a-z0-9]$", var.project_id))
@@ -20,8 +20,8 @@ variable "project_id" {
 
 variable "randomize_project_id" {
   type        = bool
-  default     = true
-  description = ""
+  default     = false
+  description = "If true, adds a 6-digit random string preceded by a dash. The 'project_id' variable cannot then be longer than 23 characters."
 }
 
 variable "parent" {
@@ -37,29 +37,23 @@ variable "parent" {
 variable "billing_account" {
   type        = string
   default     = null
-  description = ""
+  description = "The alphanumeric ID of the billing account this project belongs to."
 }
 
 variable "skip_delete" {
   type        = bool
   default     = false
-  description = ""
+  description = "If true, the Terraform resource can be deleted without deleting the project."
 }
 
 variable "labels" {
   type        = map(string)
   default     = {}
-  description = ""
-}
-
-variable "effective_labels" {
-  type        = map(string)
-  default     = {}
-  description = ""
+  description = "A map of key/value label pairs to assign to the project."
 }
 
 variable "create_default_network" {
   type        = bool
   default     = true
-  description = ""
+  description = "Controls whether the 'default' network exists on the project. If false, the default network will still be created by GCP but will be deleted immediately by Terraform."
 }
