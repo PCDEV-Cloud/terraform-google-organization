9f2c31b6dace15021ec536a4f27807d6c4da8c7c
Paweł Cholewa <pawel.cholewa@pcdev.pl>
2023-12-07 14:16:51 +0100

    Add project submodule to root module

--- variables.tf
+++ variables.tf
@@ -6,3 +6,9 @@ variable "organization" {
 variable "folders" {
   description = "A list of folders with their subfolders. Maximum nesting is limited to 9 levels."
 }
+
+variable "projects" {
+  type        = list(any)
+  default     = []
+  description = "A list of projects with settings."
+}
