4cd0139587d829251a98eb3b5e7e902f477906a6
Paweł Cholewa <pawel.cholewa@pcdev.pl>
2023-12-08 11:15:42 +0100

    Add outputs to project submodule

--- modules/project/outputs.tf
+++ modules/project/outputs.tf
@@ -1,9 +1,29 @@
+output "folder_id" {
+  value       = google_project.this.folder_id
+  description = ""
+}
+
 output "id" {
   value       = google_project.this.id
   description = "An identifier for the resource with format 'projects/<ID>'."
 }
 
+output "name" {
+  value       = google_project.this.name
+  description = ""
+}
+
 output "number" {
   value       = google_project.this.number
   description = "The numeric identifier of the project."
+}
+
+output "org_id" {
+  value       = google_project.this.org_id
+  description = ""
+}
+
+output "project_id" {
+  value       = google_project.this.project_id
+  description = ""
 }
