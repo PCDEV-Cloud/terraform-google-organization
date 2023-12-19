a9633dd9cb78ae03782b2b5aacd883915f8a07c5
Paweł Cholewa <pawel.cholewa@tsh.io>
2023-12-19 22:41:09 +0100

    Add project services

--- modules/project/main.tf
+++ modules/project/main.tf
@@ -24,3 +24,19 @@ resource "google_project" "this" {
   labels              = var.labels
   auto_create_network = var.create_default_network
 }
+
+resource "google_project_service" "this" {
+  for_each = toset(var.apis_and_services)
+
+  project                    = google_project.this.id
+  service                    = each.value
+  disable_dependent_services = true
+  disable_on_destroy         = true
+
+  timeouts {
+    create = "20m"
+    read   = "10m"
+    update = "20m"
+    delete = "20m"
+  }
+}
