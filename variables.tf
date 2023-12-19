f29aa999e4bc6127c7f204b98467532e7c7ff3a5
Paweł Cholewa <pawel.cholewa@pcdev.pl>
2023-12-19 23:05:24 +0100

    Update root module vars

--- variables.tf
+++ variables.tf
@@ -4,11 +4,11 @@ variable "organization" {
 }
 
 variable "folders" {
+  default     = []
   description = "A list of folders with their subfolders. Maximum nesting is limited to 9 levels."
 }
 
 variable "projects" {
-  type        = list(any)
   default     = []
   description = "A list of projects with settings."
 }
